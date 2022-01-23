d1f2fef9d2313fed9928912617285270c299e782
lanly_dev <lanly-dev@users.noreply.github.com>
2022-01-22 22:28:56 -0800

    feat: local convert is working

--- vscode-emc/src/converter.ts
+++ vscode-emc/src/converter.ts
@@ -1,41 +1,92 @@
-import { window } from 'vscode'
-import pathToFfmpeg = require('ffmpeg-static')
+import { resolve } from 'path'
+import { Uri, window } from 'vscode'
+import * as os from 'os'
+import axios from 'axios'
 import ffmpeg = require('fluent-ffmpeg')
+import pathToFfmpeg = require('ffmpeg-static')
+import { createWriteStream } from 'fs'
+ffmpeg.setFfmpegPath(pathToFfmpeg)
+const pkg = require('ffmpeg-static/package.json')
+import * as stream from 'stream'
+import { promisify } from 'util'
 
 export default class Converter {
-
-  static async convert(path: string) {
-    this.printToChannel('hello')
+  static async download() {
+    console.log('hello')
+    const {
+      'ffmpeg-static': { 'binary-release-tag': rTag, 'binary-release-name': rName }
+    } = pkg
+    const arch = os.arch()
+    const platform = os.platform()
+    const baseUrl = `https://github.com/eugeneware/ffmpeg-static/releases/download/${rTag ?? rName}`
+    const url = `${baseUrl}/${platform}-${arch}`
+    await this.downloadStream(url)
+    console.log('Hello')
   }
 
-  static async convertLocal(path: string) {
-    const ffmpeg = require('fluent-ffmpeg')
-    ffmpeg.setFfmpegPath(pathToFfmpeg)
-
-    // await ffmpegConvert()
+  static async convert({ fsPath, path }: Uri, type: 'mp3' | 'mp4') {
+    try {
+      this.printToChannel(path)
+      const name = path.split('/').pop()
+    } catch (error) {
+      //@ts-ignore
+      window.showErrorMessage(error.message ?? error)
+    }
   }
 
-  private ffmpegConvert(format: string, input: string, output: string) {
+  static async convertLocal({ fsPath, path }: Uri, type: 'mp3' | 'mp4') {
+    try {
+      this.printToChannel(path)
+      const fileName = path.split('/').pop()
+      //@ts-ignore
+      const [name, ext] = fileName?.split('.')
+      const oFileName = `${name}.${type}`
+      const oPath = fsPath.replace(ext, type)
+      console.log(fsPath, path)
+      console.log(oPath)
+      await this.ffmpegConvert(type, fsPath, oPath)
+    } catch (error) {
+      //@ts-ignore
+      window.showErrorMessage(error.message ?? error)
+    }
+  }
+
+  private static ffmpegConvert(type: string, input: string, output: string) {
     return new Promise<void>((resolve, reject) => {
       ffmpeg(input)
         .on('progress', (progress) => {
-          console.log(`[ffmpeg] ${JSON.stringify(progress)}`)
+          this.printToChannel(`[ffmpeg] ${JSON.stringify(progress)}`)
         })
         .on('error', (err) => {
-          console.log(`[ffmpeg] error: ${err.message}`)
+          this.printToChannel(`[ffmpeg] error: ${err.message}`)
           reject(err)
         })
         .on('end', () => {
-          console.log('[ffmpeg] finished')
+          this.printToChannel('[ffmpeg] finished')
           resolve()
         })
-        .format(format)
+        .format(type)
         .save(output)
     })
   }
 
+  private static downloadStream(url: string) {
+    const { onDownloadProgress } = this
+    console.log(__dirname)
+    const writer = createWriteStream(resolve(__dirname, 'ffmpeg'))
+    axios.get(url, { responseType: 'stream', onDownloadProgress }).then((response) => {
+      console.log(response)
+      response.data.pipe(writer)
+      return promisify(stream.finished)(writer)
+    })
+  }
+
+  private static onDownloadProgress(event: any) {
+    console.log(event)
+  }
+
   private static printToChannel(output: string) {
-    const channel = window.createOutputChannel('dictionary')
+    const channel = window.createOutputChannel('Easy Media Converter')
     channel.append(output)
     channel.show()
   }
--- vscode-emc/src/extension.ts
+++ vscode-emc/src/extension.ts
@@ -1,13 +1,15 @@
-import { ExtensionContext, commands } from 'vscode'
+import { ExtensionContext, commands, Uri } from 'vscode'
 import Converter from './converter'
 
 export function activate(context: ExtensionContext) {
   const rc = commands.registerCommand
+  console.log('$$$$$$$$$$$')
+  Converter.download()
   context.subscriptions.concat([
-    rc('emc.convertMp3', (path: string) => Converter.convert(path)),
-    rc('emc.convertMp4', (path: string) => Converter.convert(path)),
-    rc('emc.convertLocalMp3', (path: string) => Converter.convertLocal(path)),
-    rc('emc.convertLocalMp4', (path: string) => Converter.convertLocal(path))
+    rc('emc.convertMp3', (uri: Uri) => Converter.convert(uri, 'mp3')),
+    rc('emc.convertMp4', (uri: Uri) => Converter.convert(uri, 'mp3')),
+    rc('emc.convertLocalMp3', (uri: Uri) => Converter.convertLocal(uri, 'mp4')),
+    rc('emc.convertLocalMp4', (uri: Uri) => Converter.convertLocal(uri, 'mp4'))
   ])
 }
 
